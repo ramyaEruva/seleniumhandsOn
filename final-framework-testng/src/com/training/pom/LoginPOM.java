a0ec1a7f448fc9507c0eb6a677863eabf34136d1
Eruva Ramya <eruramya@in.ibm.com>
2019-09-24 19:47:02 +0530

    LoginPOM.java

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.training.sanity.tests.dropdown;
+import com.training.sanity.tests.user;
+
 public class LoginPOM {
 	private WebDriver driver; 
+	String userNm;
+	String pwd;
 	
 	public LoginPOM(WebDriver driver) {
 		this.driver = driver; 
@@ -19,20 +24,38 @@ public class LoginPOM {
 	@FindBy(id="password")
 	private WebElement password;
 	
-	@FindBy(id="formLogin_submitAuth")
+	@FindBy(id="form-login_submitAuth")
 	private WebElement loginBtn; 
+	@FindBy(linkText="Site Owner")
+	private WebElement dropDown;
 	
 	public void sendUserName(String userName) {
 		this.userName.clear();
 		this.userName.sendKeys(userName);
+		userNm=userName;
 	}
+	public String getUserName() {
+		
+		return userNm;
+	}
+		
 	
 	public void sendPassword(String password) {
 		this.password.clear(); 
 		this.password.sendKeys(password); 
+		pwd=password;
+	}
+	public String getPassword() {
+		
+		return pwd;
 	}
 	
 	public void clickLoginBtn() {
 		this.loginBtn.click(); 
 	}
+	public void clickDropDown()
+	{
+		this.dropDown.click();
+	}
+
 }
